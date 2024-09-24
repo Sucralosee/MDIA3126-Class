571b6d95c4a30d8f7a20f5a6ef57f2aa306cd156
Ben Louis <142682578+Sucralosee@users.noreply.github.com>
2024-09-24 13:44:15 -0700

    testing with nativewind - currently does not work

---
+++ NativeWind-Test-App/app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import "../../global.css"
+
+
+
+export default function HomeScreen() {
+  return (
+    <View className="bg-slate-700">
+      <Text className="text-white text-4xl">Hi</Text>
+    </View>
+  );
+}
