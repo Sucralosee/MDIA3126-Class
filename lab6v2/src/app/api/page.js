c803a1ab90d26fd697e5fadd76aa980ac9f24099
Ben Louis <142682578+Sucralosee@users.noreply.github.com>
2024-10-08 16:04:51 -0700

    added NASA and MDM product API's

--- lab6v2/src/app/api/page.js
+++ lab6v2/src/app/api/page.js
@@ -1,9 +1,109 @@
+"use client"
+//API Page Declaration
 import Image from "next/image";
+import { useState } from "react";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Hello
-    </div>
-  );
+export default function Page() {
+    const DATA_URL = "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json";
+    const [data, setData] = useState(null);
+
+    async function fetchData() {
+        const response = await fetch(DATA_URL);
+        console.log(response);
+        const data = await response.json();
+        setData(data);
+    }
+
+    //nasa time
+    const NASA_API = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=3";
+
+    async function fetchNASA() {
+        const response = await fetch(NASA_API);
+        console.log(response);
+        const data = await response.json();
+        setData(data);
+    }
+
+    const DisplayMedia = () => {
+        if (data){
+          let mediaList = [];
+          data.forEach((medium, index) => {
+            let formattedMedia = medium.media_type === 'image' ? <img src={medium.url} /> : (
+            <video controls width="250">
+              <source src={medium.url} type="vide/webm"/>
+            </video>
+            )
+
+            mediaList.push(
+              <li key={index}>
+                {formattedMedia}
+                <h2>{medium.title} </h2>
+                {medium.explanation}
+              </li>
+            );
+          });
+
+            return(
+                <div>
+                    <ul>
+                        {mediaList}
+                    </ul>
+                </div>
+            )
+        }else{
+            return(
+                <div>nah</div>
+            )
+        }
+    }
+ 
+
+    const DisplayProducts = () => {
+        if (data) {
+
+            const productsList = [];
+            data.forEach((product, index) => {
+                productsList.push(
+                    <li key={index}>{product.name}</li>
+                )
+            });
+
+
+            return (
+                <div className="border-4 border-black p-4 mb-4">
+                    <ul >
+                        {productsList}
+                    </ul>
+                </div>
+            )
+        } else {
+            return (
+                <div className="border-4 border-black p-4 mb-4">
+                    <ul >
+                        Nothing
+                    </ul>
+                </div>
+            )
+        }
+    }
+
+    return (
+        <div className="p-4">
+            <header className="border-4 border-white p-4 mb-4">
+                <h1>Welcome to my NASA Page</h1>
+                <button
+                    className="border-neutral-200 bh-black px-6"
+                onClick={() => fetchData()}
+                >check product</button>
+                <button                     
+                    className="border-neutral-200 bh-black px-6"
+                    onClick={() => fetchNASA()}
+                >check out the image of the day</button>
+
+            </header>
+            Welcome to my API Page
+            <DisplayProducts />
+            <DisplayMedia />
+        </div>
+    );
 }
--- lab6v2/src/app/page.js
+++ lab6v2/src/app/page.js
@@ -1,9 +1,15 @@
 import Image from "next/image";
 
 export default function Home() {
+
+  const DATA_URL = "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json";
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      World
+    // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="p-4 bg-green-300">
+      <h1>Welcom to my product page</h1>
+      
     </div>
   );
 }
+// https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json
